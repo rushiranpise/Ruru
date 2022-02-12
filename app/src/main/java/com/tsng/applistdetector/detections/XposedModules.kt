f80e8dcc1490261375613e3db6a944637d0d0b0f
by小润 <31344394+byxiaorun@users.noreply.github.com>
2022-02-13 01:39:26 +0800

    fix XposedModules.kt

--- app/src/main/java/com/tsng/applistdetector/detections/XposedModules.kt
+++ app/src/main/java/com/tsng/applistdetector/detections/XposedModules.kt
@@ -11,14 +11,20 @@ object XposedModules : IDetector() {
     override fun execute() {
         results.clear()
         val packages = mutableSetOf<String>()
-
         val pm = appContext.packageManager
         val intent = pm.getInstalledApplications(PackageManager.GET_META_DATA)
         for (pkg in intent) {
             if (pkg.metaData?.get("xposedmodule") != null)
                 packages.add(pm.getApplicationLabel(pkg) as String)
         }
-
+        if (packages.isNullOrEmpty()){
+            val intent = pm.queryIntentActivities(Intent(Intent.ACTION_MAIN),PackageManager.GET_META_DATA)
+            for (pkg in intent) {
+                val ainfo=pkg.activityInfo.applicationInfo
+                if (ainfo.metaData?.get("xposedmodule") != null)
+                    packages.add(pm.getApplicationLabel(ainfo) as String)
+            }
+        }
         packages.forEach { results.add(Pair(it, Results.FOUND)) }
     }
 }
